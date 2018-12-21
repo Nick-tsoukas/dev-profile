2b5a537c374e97da2e2dc303bb71c06599b28098
Nick Tsoukas <nick.tsoukas101@gmail.com>
2018-12-21 16:10:25 -0700

    Adding profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,12 +6,7 @@ const passport = require('passport');
 const Profile = require('../../models/Profile');
 const User = require('../../models/Users');
 
-router.get('/test', (req, res) => {
-  res.json({
-    msg: "profile works"
-  });
-});
-
+//passport will return the user object in the payload
 router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
   const errors = {};
 
@@ -23,6 +18,7 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
       }
       res.json(profile);
     })
+    .catch(err => res.status(404).json(err));
 });
 
 module.exports = router;
